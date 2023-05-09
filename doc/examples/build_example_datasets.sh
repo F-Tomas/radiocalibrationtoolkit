7d98202568916ea071fd5727f0b3bc8695b5a60d
t.fodran <t.fodran@astro.ru.nl>
2023-05-09 11:17:29 +0200

    Resolve generating data 2

--- doc/examples/build_example_datasets.sh
+++ doc/examples/build_example_datasets.sh
@@ -2,73 +2,76 @@
 
 GMAPS='LFSS GSM08 GSM16 Haslam LFmap SSM GMOSS ULSA'
 
-python ./utilities/create_isotropic_antenna_pattern.py
-python ./utilities/create_flat_hw_profile.py
+#base=./
+base=./examples
+
+python ${base}/utilities/create_isotropic_antenna_pattern.py
+python ${base}/utilities/create_flat_hw_profile.py
 
 # simulated power in HW
 for GMAP in $GMAPS; do
     echo $GMAP;
-    python ./utilities/get_power_dataset.py -g $GMAP -a ./antenna_setup_files/SALLA_EW.xml -w ./antenna_setup_files/HardwareProfileList_realistic.xml -s Salla;
+    python ${base}/utilities/get_power_dataset.py -g $GMAP -a ${base}/antenna_setup_files/SALLA_EW.xml -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -s Salla;
     echo =================================================================
 done
     
 for GMAP in $GMAPS; do
     echo $GMAP;
-    python ./utilities/get_power_dataset.py -g $GMAP -a ./antenna_setup_files/Isotropic_antenna_pattern.xml -w ./antenna_setup_files/HardwareProfileList_flat.xml -s isoAnt_flathw;
+    python ${base}/utilities/get_power_dataset.py -g $GMAP -a ${base}/antenna_setup_files/Isotropic_antenna_pattern.xml -w ${base}/antenna_setup_files/HardwareProfileList_flat.xml -s isoAnt_flathw;
     echo =================================================================
 done
 
 # simulated voltage square density
 for GMAP in $GMAPS; do
     echo $GMAP;
-    python ./utilities/get_power_dataset.py -g $GMAP -a ./antenna_setup_files/SALLA_EW.xml -w -s Salla_EW -o ./voltage2_density/ -t voltage2 -f 10 125;
+    python ${base}/utilities/get_power_dataset.py -g $GMAP -a ${base}/antenna_setup_files/SALLA_EW.xml -w -s Salla_EW -o ${base}/voltage2_density/ -t voltage2 -f 10 125;
     echo =================================================================
 done
     
 for GMAP in $GMAPS; do
     echo $GMAP;
-    python ./utilities/get_power_dataset.py -g $GMAP -a ./antenna_setup_files/Isotropic_antenna_pattern.xml -s isoAnt -o ./voltage2_density/ -t voltage2 -f 10 125;
+    python ${base}/utilities/get_power_dataset.py -g $GMAP -a ${base}/antenna_setup_files/Isotropic_antenna_pattern.xml -s isoAnt -o ${base}/voltage2_density/ -t voltage2 -f 10 125;
     echo =================================================================
 done
 
 # mock power datasets
 for GMAP in $GMAPS; do
     echo $GMAP;
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_$GMAP.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_$GMAP.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0
     echo =================================================================
 done
 
 for GMAP in $GMAPS; do
     echo $GMAP;
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_isoAnt_$GMAP.csv -w ./antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_isoAnt_$GMAP.csv -w ${base}/antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0
     echo =================================================================
 done
 
 # various size
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 5000 -a 0 -tor
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 1000 -a 0 -tor
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 5000 -a 0 -tor
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 1000 -a 0 -tor
 
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 5000 -a 0
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 1000 -a 0
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 5000 -a 0
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 1000 -a 0
 
 ### Constant temperature ###
 ## without rounding, 
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0 -tor -t 30
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0 -tor -t 30
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0 -tor -t 30
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0 -tor -t 30
 
 # with extra noise
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 5e-16 -tor -t 30
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 5e-16 -tor -t 30
 
 
 ## with rounding 
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0 -t 30
-python ./utilities/get_mock_dataset.py -v ./voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ./antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0 -t 30
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_Salla_EW_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_realistic.xml -n 10000 -a 0 -t 30
+python ${base}/utilities/get_mock_dataset.py -v ${base}/voltage2_density/voltage2_density_isoAnt_GSM16.csv -w ${base}/antenna_setup_files/HardwareProfileList_flat.xml -n 10000 -a 0 -t 30
 
 
 
-# python convertSfile2xml.py -i ./Sparamaters/RD_mean_LNA_EW.s2p -s HardwareProfileList_realistic.xml -m w -l LNA
-# python convertSfile2xml.py -i ./Sparamaters/Cable_assembly_ant-dig_with_pigtail.s2p -s HardwareProfileList_realistic.xml -m a -l cable_fromLNA2digitizer
-# python convertSfile2xml.py -i ./Sparamaters/Digitizer_v3_high_gain_MagAngle_dB_deg.s2p -s HardwareProfileList_realistic.xml -m a -l digitizer
+# python convertSfile2xml.py -i ${base}/Sparamaters/RD_mean_LNA_EW.s2p -s HardwareProfileList_realistic.xml -m w -l LNA
+# python convertSfile2xml.py -i ${base}/Sparamaters/Cable_assembly_ant-dig_with_pigtail.s2p -s HardwareProfileList_realistic.xml -m a -l cable_fromLNA2digitizer
+# python convertSfile2xml.py -i ${base}/Sparamaters/Digitizer_v3_high_gain_MagAngle_dB_deg.s2p -s HardwareProfileList_realistic.xml -m a -l digitizer
 
 
 

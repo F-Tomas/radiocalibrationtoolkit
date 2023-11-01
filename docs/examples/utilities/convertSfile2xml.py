a6d8989b9d6488e66db0223f5aaac041e9223132
t.fodran <t.fodran@astro.ru.nl>
2023-11-01 17:09:38 +0100

    Update sparam2xml conversion script

--- docs/examples/utilities/convertSfile2xml.py
+++ docs/examples/utilities/convertSfile2xml.py
@@ -1,252 +1,204 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  convertSParameterFile2XML.py
 
-import sys
+import argparse
+import re
+import io
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import skrf as rf
-from pathlib import Path
-from radiocalibrationtoolkit import *
-
-MHz = 1e6
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
 
 
-def nparray2string(arr):
-    string = ""
-    for i in arr:
-        # string+='{:.2f} '.format(i)
-        string += "{} ".format(round(i, 2))
-    return string
+def main(args):
+    np.set_printoptions(suppress=True)
+    skrf = rf.Network(sparameterFilePath)
+    MHz = 1e-6
+    frequencyMHz = skrf.f * MHz
 
+    if method == 'withS11':
+        complex_voltage_amplification = (skrf.s21.s/(1+skrf.s11.s)).flatten()
+    elif method == 'withoutS11':
+        complex_voltage_amplification = (skrf.s21.s/(1)).flatten()
+    dBMagnitude = 20*np.log10(np.abs(complex_voltage_amplification))
+    
+    phaseDegreeWrapped = np.rad2deg(np.angle(complex_voltage_amplification))
 
-def get_hw_profile_xml_string(
-    responseId,
-    freq_string,
-    mag_string,
-    phase_string,
-    mag_unit="dB",
-    phase_unit="degree",
-    type_="Response",
-):
-    freq_string = nparray2string(freq_string)
-    mag_string = nparray2string(mag_string)
+    # unwrape phase if wraped
+    phaseDegreeUnwrapped = np.unwrap(phaseDegreeWrapped, 180)
 
-    header = "<!-- {} -->".format(ifilename)
-
-    freqUnit = "MHz"
-
-    type__ = type_[0] + type_
-
-    if phase_string is not None:
-        phase_string = nparray2string(phase_string)
-        phase_XML_string = (
-            '      <Phase unit ="' + phase_unit + '">\n'
-            "        " + phase_string + "      </Phase>\n"
-        )
+    if useUnwrappedPhase == True:
+        phaseDeg = phaseDegreeUnwrapped
     else:
-        phase_XML_string = ""
+        phaseDeg = phaseDegreeWrapped
 
-    xml_string = (
+    #if rounding != None:
+     #   dBMagnitude = np.asarray([round(i, rounding) for i in dBMagnitude])
+   #     phaseDeg = np.asarray([round(i, rounding) for i in phaseDeg])
+
+    # save in the Offline XML format
+    freqString = re.sub(
+        " +",
+        " ",
+        np.array2string(
+            frequencyMHz, separator=" ", max_line_width=np.inf, suppress_small=False, formatter={'float_kind': lambda x: f'{x:.{rounding}f}'}
+        ).strip("[,]"),
+    )
+    magString = re.sub(
+        " +",
+        " ",
+        np.array2string(
+            dBMagnitude, separator=" ", max_line_width=np.inf, suppress_small=False, formatter={'float_kind': lambda x: f'{x:.{rounding}f}'}
+        ).strip("[,]"),
+    )
+    phaseString = re.sub(
+        " +",
+        " ",
+        np.array2string(
+            phaseDeg, separator=" ", max_line_width=np.inf, suppress_small=False,  formatter={'float_kind': lambda x: f'{x:.{rounding}f}'}
+        ).strip("[,]"),
+    )
+
+    # header = "<!-- {} -->".format(responseName)
+    header = ''
+    
+    responseId = responseName
+
+    freqUnit = "MHz"  # 'MHz'
+    magUnit = (
+        "dB"
+    )  # 'LgAmp'  dB::: <!-- amplification in dB -->  LgAmp::: <!-- amplification in Log10(Amplitude) -->
+    phaseUnit = "degree"  #'degree' # 'radiands' ?
+
+    string = (
         header
-        + "\n"
-        + "  <"
-        + type__
-        + ' id="'
+        + "\n\n\n"
+        + '  <RResponse id="'
         + responseId
         + '"> \n'
-        + "    <"
-        + type_
-        + "> \n "
+        + "    <Response> \n "
         + '      <x unit="'
         + freqUnit
         + '"> \n'
         + "        "
-        + freq_string
+        + freqString
         + "      </x>\n"
         + "      <"
-        + mag_unit
+        + magUnit
         + ">\n"
         + "        "
-        + mag_string
+        + magString
         + "      </"
-        + mag_unit
+        + magUnit
         + ">\n"
-        + phase_XML_string
-        + "    </"
-        + type_
-        + ">\n"
-        + "  </"
-        + type__
-        + ">"
-        + "\n\n\n"
-    )
-    return xml_string
-
-
-def calculate_impedance(c):
-    return args["reference_impedance"] * (1 + c) / (1 - c)
-
-
-def s2impedance(sparameter_file_path, frequency_MHz_new=None):
-    ntwk = rf.Network(sparameter_file_path)
-    if frequency_MHz_new != None:
-        ntwk = ntwk.interpolate(frequency_MHz_new, fill_value="extrapolate")
-    return ntwk.f / MHz, np.real(
-        np.asarray(list(map(calculate_impedance, ntwk.s11.s.flatten())))
+        + '      <Phase unit ="'
+        + phaseUnit
+        + '">\n'
+        "        "
+        + phaseString
+        + "      </Phase>\n"
+        + "    </Response>\n"
+        + "  </RResponse>"
     )
 
+    with open(os.path.join(outdir, responseName + ".xml"), "w") as file:
+        file.write(string)
 
-def s2dB(sparameter_file_path, unwrapped_phase=True, frequency_MHz_new=None):
-    ntwk = rf.Network(sparameter_file_path)
-    if frequency_MHz_new != None:
-        ntwk = ntwk.interpolate(frequency_MHz_new, fill_value="extrapolate")
+    # control plots
+    '''
+    plt.figure(0)
+    skrf.plot_s_db()
+    plt.figure(1)
+    skrf.plot_s_deg()
 
-    frequency_MHz = ntwk.f / MHz
-    magnitude_dB = ntwk.s21.s_db.flatten()
-    phase_degs_wrapped = ntwk.s21.s_deg.flatten()
-
-    # unwrape phase if wraped
-    phase_degs_unwrapped = np.unwrap(phase_degs_wrapped, 180)
-
-    if unwrapped_phase:
-        phase_degs = phase_degs_unwrapped
-    else:
-        phase_degs = phase_degs_wrapped
-
-    magnitude_dB = np.around(magnitude_dB, args["round"])
-    phase_degs = np.around(phase_degs, args["round"])
-
-    return frequency_MHz, magnitude_dB, phase_degs
-
-
-def main():
-    if args["frange"] != None:
-        frequency_MHz_new = rf.Frequency(
-            args["frange"][0],
-            args["frange"][1],
-            args["frange"][1] - args["frange"][0] + 1,
-            "MHz",
-        )
-    else:
-        frequency_MHz_new = None
-
-    frequency_MHz, values_dB, values_phase_degs = s2dB(
-        args["inputfile"], frequency_MHz_new=frequency_MHz_new
-    )
-    amp_xml_string = get_hw_profile_xml_string(
-        label,
-        frequency_MHz,
-        values_dB,
-        values_phase_degs,
-        mag_unit="dB",
-        phase_unit="degree",
-    )
-
-    frequency_MHz, impedance = s2impedance(
-        args["inputfile"], frequency_MHz_new=frequency_MHz_new
-    )
-    imp_string = get_hw_profile_xml_string(
-        label,
-        frequency_MHz,
-        impedance,
-        None,
-        mag_unit="y",
-        phase_unit="degree",
-        type_="Impedance",
-    )
-
-    abs_output_path = os.path.join(args["outputpath"], args["savefilename"])
-    closing_tags = "</HardwareProfileList>"
-    # if appending, delete closing tags and do not use the opening tags
-    if args["mode"] == "a":
-        opening_tags = ""
-        with open(abs_output_path, "r+") as f:
-            file_contents = f.read()
-            file_contents = file_contents.replace(closing_tags, "")
-            f.seek(0)
-            f.write(file_contents)
-            f.truncate()
-    else:
-        opening_tags = "<HardwareProfileList>\n\n"
-
-    with open(abs_output_path, args["mode"]) as f:
-        f.write(opening_tags + amp_xml_string + imp_string + closing_tags)
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    fig.suptitle("what goes to the converted XML file")
+    ax[0].plot(frequencyMHz, dBMagnitude)
+    ax[1].plot(frequencyMHz, phaseDeg)
+    [a.set_xlabel("frequency [MHz]") for a in ax]
+    ax[0].set_ylabel("dB")
+    ax[1].set_ylabel("phase [deg]")
+    plt.subplots_adjust(wspace=0.4)
+    plt.show()
+    '''
+    return 0
 
 
 if __name__ == "__main__":
+    import sys
+    import os
+
+    # Construct the argument parser
     ap = argparse.ArgumentParser()
     ap._action_groups.pop()
     required = ap.add_argument_group("required arguments")
     optional = ap.add_argument_group("optional arguments")
 
+    # Add the arguments to the parser
     required.add_argument(
-        "-i",
-        "--inputfile",
-        nargs="?",
-        required=True,
-        help="Path to the S-parameter file.",
+        "-s", "--sfile", required=True, help="Path to the s-parameter file."
     )
-    required.add_argument(
-        "-m",
-        "--mode",
-        nargs="?",
-        required=True,
-        choices=["w", "a"],
-        help="Append or Write to an existing file.",
-    )
-    optional.add_argument(
-        "-l",
-        "--label",
-        nargs="?",
-        default="",
-        help="Label for the hardware. If not provided, stem from the file name will be used.",
-    ),
-    optional.add_argument(
-        "-f",
-        "--frange",
-        nargs="+",
-        default=None,
-        type=int,
-        help="Use skrf to interpolate the s-parameter file values to a frequency range in MHz, 'from' 'to'.\
-              Must be an integer!. Spacing is '1'",
-    ),
     optional.add_argument(
         "-o",
-        "--outputpath",
+        "--output_name",
         nargs="?",
-        default="./antenna_setup_files/",
-        help="Output folder. If it does not exists, it will be created.",
+        required=False,
+        default=None,
+        help="File name of the produced XML file. Also it is the name of the HW response. By default filename of the s-parameter file will be used.",
     )
     optional.add_argument(
-        "-s",
-        "--savefilename",
+        "-d",
+        "--output_dir",
         nargs="?",
-        default="HardwareProfileList.xml",
-        help="Saved power dataset filename.",
+        required=False,
+        default='./',
+        help="Output directory",
+    )
+    optional.add_argument(
+        "-u",
+        "--unwrap",
+        nargs="?",
+        required=False,
+        default=True,
+        help="Unwrap phase. Default is True.",
+    )
+    optional.add_argument(
+        "-m",
+        "--method",
+        nargs="?",
+        required=False,
+        default='withS11',
+        help="The amplification is either given as S21/(S11+1) or as just S21. Thus our options are 'withS11' or 'withoutS11. Default: 'withS11",
     )
     optional.add_argument(
         "-r",
-        "--round",
-        nargs=1,
-        default=3,
-        type=int,
-        help="To how many places the hw profiles will be rounded..",
+        "--rounding",
+        nargs="?",
+        required=False,
+        default=4,
+        help="Rouding of the save numbers. Default is 4. For no rouding type None",
     )
-    optional.add_argument(
-        "-z",
-        "--reference_impedance",
-        nargs=1,
-        default=50,
-        type=float,
-        help="Reference impedance, default is 50 Ohms.",
-    )
-
     args = vars(ap.parse_args())
 
-    ifilename = Path(args["inputfile"]).stem
-    if args["label"] == "":
-        label = ifilename
+    method = args['method']
+    sparameterFilePath = args["sfile"]
+    useUnwrappedPhase = args["unwrap"]
+    rounding = int(args["rounding"])
+
+    if args["output_name"] == None:
+        responseName = os.path.splitext(os.path.basename(sparameterFilePath))[0]
     else:
-        label = args["label"]
+        responseName = args["output_name"]
 
-    mkdir(args["outputpath"])
+    outdir = args["output_dir"]
+    # workDir ='/home/tomas/Documents/scripts/pyUtilScripts/'
+    # inputFileName = 'AugerRD_LNA_GRF4002_NS_39mA_ReIm.s2p'
+    # sparameterFilePath = workDir+inputFileName
+    # useUnwrappedPhase = True
+    # rounding = 3
 
-    sys.exit(main())
+    sys.exit(main(sys.argv))
